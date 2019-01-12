00458fd888ceb3ce9430fb024f85003ac0c3c876
Jim Eli <31859133+JimEli@users.noreply.github.com>
2019-01-12 07:05:56 -0700

    Update main.c

--- CIS265_2B/main.c
+++ CIS265_2B/main.c
@@ -11,7 +11,7 @@
 * (In case you don’t have a telephone nearby, here are the letters on the 
 * keys: 2=ABC, 3=DEF, 4=GHI, 5=JKL, 6=MNO, 7=PRS, 8=TUV, 9=WXY.) If the 
 * original phone number contains nonalphabetic characters (digits or 
-* punctuation, for example), leave then unchanged:
+* punctuation, for example), leave them unchanged:
 *
 * Notes:
 *  (1) Compiled with MS Visual Studio 2017 Community (v141), using C
@@ -33,45 +33,54 @@
 #define MAX_PHONE_NUMBER_STRING_LENGTH 24 // Maximum length of input string.
 
 // Convert phone character to number.
-char toNumber(char c) {
+char toNumber(char c) 
+{
 	char _c = tolower(c); // Use lower case for comparisons.
 
 	// Check for non-phone character.
 	if (_c < 'a' || _c > 'z' || _c == 'q')
 		return c;
+
 	// Special case of 'z'.
 	if (_c == 'z') 
 		return '9';
+	
 	// Apply adjustemnt for missing 'q'.
 	if (_c > 'p')
 		_c -= 1;
+	
 	// Convert and return appropriate number.
 	return '0' + (_c - 'a') / 3 + 2;
 }
 
 // Program starts here.
-int main(void) {
+int main(void) 
+{
 	int attempts = MAX_INPUT_ATTEMPTS;      // Input attempt counter.
 	// Attempts must be valid non-zero, positive number.
 	assert(attempts > 0);
 
 	// Attempt only so many inputs.
-	while (attempts--) {
+	while (attempts--) 
+	{
 		char s[MAX_PHONE_NUMBER_STRING_LENGTH]; // Holds input phone number.
 												
 		// Prompt and grab user input.
 		fputs("Enter phone number: ", stdout);
-		if (!fgets(s, MAX_PHONE_NUMBER_STRING_LENGTH - 2, stdin)) {
+		if (!fgets(s, MAX_PHONE_NUMBER_STRING_LENGTH - 2, stdin)) 
+		{
 			fputs("Fatal program error!\n", stderr);
 			exit(EXIT_FAILURE);
 		}
-		else if (!strchr(s, '\n')) {
+		else if (!strchr(s, '\n')) 
+		{
 			// input too long, eat remainder.
 			while (fgets(s, MAX_PHONE_NUMBER_STRING_LENGTH - 2, stdin) && !strchr(s, '\n'))
 				; // Empty body.
 			fputs("Too many characters input.\n", stdout);
 		}
-		else {
+		else 
+		{
 			// Convert and display.
 			for (unsigned int i = 0; i < strlen(s); printf("%c", toNumber(s[i++])));
 			break;
